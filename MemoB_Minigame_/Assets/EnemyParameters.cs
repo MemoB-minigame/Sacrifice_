d0b0a628a66dabb37889719c4b6471306f679095
Inoriant <424504326@qq.com>
2022-10-23 22:38:55 +0800

    创建了具有自动追踪玩家和站立两个状态的敌人

---
+++ MemoB_Minigame_/Assets/EnemyParameters.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class EnemyParameters :MonoBehaviour
+{
+    //public GameObject[] MovePoints;
+    public GameObject Player;
+}
