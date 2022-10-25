64b512169baee6d78c67f08e59e390fa1c001929
zidan1123 <angzhenbin12181102@gmail.com>
2022-10-25 10:18:48 +0800

    人物动画(美术素材待修改)

--- MemoB_Minigame_/Assets/Scripts/PlayerController.cs
+++ MemoB_Minigame_/Assets/Scripts/PlayerController.cs
@@ -8,14 +8,20 @@ public class PlayerController : MonoBehaviour
     //Components
     private Transform m_Transform;
     private Rigidbody2D m_Rigidbody2D;
+    private Animator m_Animator;
+    private SpriteRenderer m_SpriteRenderer;
 
     //Parameters
-    [SerializeField] private float speed;
     [SerializeField] private float xMoveInputDirection;
     [SerializeField] private float yMoveInputDirection;
     [SerializeField] private float attack;
 
     //[SerializeField] private float revolverBulletSpeed; ≈≤∂ØµΩŒ‰∆˜¿Ô
+
+    //Move
+    [SerializeField] private float speed;
+    private int facingDirection = 1;
+    private bool isMove = false;
 
     private bool canSkill = false;
 
@@ -28,6 +34,8 @@ public class PlayerController : MonoBehaviour
 
     //UI
     private TextMeshProUGUI hpNum;
+
+    Vector2 mousePositionWorld;
 
     public int HP
     {
@@ -60,6 +68,8 @@ public class PlayerController : MonoBehaviour
     {
         m_Transform = gameObject.GetComponent<Transform>();
         m_Rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
         revolverBullet_Prefab = Resources.Load<GameObject>("Projectiles/Player/RevolverBullet");
 
@@ -74,6 +84,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         CheckInput();
+        CheckMovementDirection();
+
+        mousePositionWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void CheckInput()
@@ -83,12 +96,36 @@ public class PlayerController : MonoBehaviour
 
         m_Rigidbody2D.velocity = new Vector2(xMoveInputDirection, yMoveInputDirection).normalized * speed;
 
-        
+        if ((xMoveInputDirection != 0 || yMoveInputDirection != 0) && !isMove) 
+        {
+            isMove = true;
+            m_Animator.Play("PlayerRun");
+        }
+        else if (xMoveInputDirection == 0 && yMoveInputDirection == 0 && isMove)
+        {
+            isMove = false;
+            m_Animator.Play("PlayerIdle");
+        }
+    }
+
+    private void CheckMovementDirection()
+    {
+        if ((facingDirection == -1 && mousePositionWorld.x - m_Transform.position.x > 0) || (facingDirection == 1 && mousePositionWorld.x - m_Transform.position.x < 0))
+        {
+            Flip();
+        }
     }
 
     private void FixedUpdate()
     {
         
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        m_SpriteRenderer.flipX = !m_SpriteRenderer.flipX;
+        //m_Transform.localScale = new Vector3(m_Transform.localScale.x * -1, 1, 1);
     }
 
     private void Dead()
