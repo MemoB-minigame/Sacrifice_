cbbdcfd365e5e558b51bb98d20206a3d31c6fb1b
cs1807cwy <43921644+cs1807cwy@users.noreply.github.com>
2022-11-17 04:23:00 +0800

    敌人预制体相关工作。
    - 白模敌人正式更名为敌人预制体
    - 完成带动画的末日神官和暗骑士预制体制作（目前未加音效）

---
+++ MemoB_Minigame_/Assets/Scripts/Enemy/射手敌人/EnemyShooterController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooterController : Enemy_Shooter_Parameters
+{
+    int shootCount = 0;
+    protected override void Start()
+    {
+        base.Start();
+    }
+    public override IEnumerator Shoot()
+    {
+        shootCount = 0;
+        while (shootCount < shootRound)
+        {
+            animator.Play("Shoot");
+            yield return new WaitForSecondsRealtime(shootDuration);
+
+        }
+        animator.Play("Alert");
+    }
+    public void AniShoot()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        GameObject bullet = ObjectPool.Instance.GetObject(attackBulletMode);
+        bullet.transform.position = transform.position;
+        bullet.transform.rotation = Quaternion.identity;
+        bullet.transform.right = direction;
+        shootCount++;
+        animator.Play("Maneuver");
+    }
+
+    public void AniShootNoTransfer()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        GameObject bullet = ObjectPool.Instance.GetObject(attackBulletMode);
+        bullet.transform.position = transform.position;
+        bullet.transform.rotation = Quaternion.identity;
+        bullet.transform.right = direction;
+        shootCount++;
+    }
+}
---
+++ MemoB_Minigame_/Assets/Scripts/Enemy/射手敌人/EnemyShooterMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooterMove : StateMachineBehaviour
+{
+    public bool facePlayer;
+    protected EnemyShooterController para;
+    protected GameObject enemy;
+    protected new Rigidbody2D rigidbody;
+    protected bool transition;
+
+    protected float shootTimer;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        para = animator.GetComponent<EnemyShooterController>();
+        enemy = animator.gameObject;
+        rigidbody = animator.GetComponent<Rigidbody2D>();
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        shootTimer += Time.deltaTime;
+        if (Vector3.Distance(para.player.transform.position, enemy.transform.position) > para.shootRetainDistance + 0.08f)
+        {
+            rigidbody.velocity = (para.player.transform.position - enemy.transform.position).normalized * para.shootMoveSpeed;
+        }
+        else if (Vector3.Distance(para.player.transform.position, enemy.transform.position) < para.shootRetainDistance - 0.08f)
+        {
+            rigidbody.velocity = (-para.player.transform.position + enemy.transform.position).normalized * para.shootMoveSpeed;
+        }
+        else
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
+        
+        // 是否固定面朝玩家方向
+        if (facePlayer)
+        {
+            if (para.player.transform.position.x < enemy.transform.position.x)
+                enemy.transform.right = Vector3.left;
+            else enemy.transform.right = Vector3.right;
+        }
+        else
+        {
+            if (rigidbody.velocity.x >= 0)
+                enemy.transform.right = Vector3.right;
+            else enemy.transform.right = Vector3.left;
+        }
+    }
+}
