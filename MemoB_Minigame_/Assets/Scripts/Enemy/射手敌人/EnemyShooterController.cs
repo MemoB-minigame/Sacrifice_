255c55a25cc7a32bd946be571c103ce02f6eeb3b
Inoriant <424504326@qq.com>
2022-11-19 01:56:54 +0800

    机关敌人完毕

--- MemoB_Minigame_/Assets/Scripts/Enemy/射手敌人/EnemyShooterController.cs
+++ MemoB_Minigame_/Assets/Scripts/Enemy/射手敌人/EnemyShooterController.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class EnemyShooterController : Enemy_Shooter_Parameters
 {
     int shootCount = 0;
+    Enemy_Shooter_Ctrl controller;
     
     protected override void Start()
     {
+        controller = GetComponent<Enemy_Shooter_Ctrl>();
         base.Start();
     }
     public override IEnumerator Shoot()
     {
-        if (Hp > 0)
+        if (controller.HP > 0)
         {
             shootCount = 0;
             while (shootCount < shootRound)
             {
-                animator.Play("Shoot");
+                if (controller.HP > 0)
+                    animator.Play("Shoot");
+                else yield break;
                 yield return new WaitForSecondsRealtime(shootDuration);
 
             }
-            animator.Play("Alert");
+            if(controller.HP>0)
+                animator.Play("Alert");
         }
     }
     public void AniShoot()
     {
-        if (Hp > 0)
+        if (controller.HP>0)
         {
             Vector3 direction = (player.transform.position - transform.position).normalized;
             GameObject bullet = ObjectPool.Instance.GetObject(attackBulletMode);
@@ -41,7 +46,7 @@ public class EnemyShooterController : Enemy_Shooter_Parameters
 
     public void AniShootNoTransfer()
     {
-        if (Hp > 0)
+        if (controller.HP > 0)
         {
             Vector3 direction = (player.transform.position - transform.position).normalized;
             GameObject bullet = ObjectPool.Instance.GetObject(attackBulletMode);
